43fed619938f8fb9459fdfae76796e3cb7b67807
Alex_Dyshevnyj <capitankl21@gmail.com>
2022-04-07 17:25:12 +0400

    Additing webpack-config. Additing gitignore. Additing ang configuration package

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/main.js":
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+/***/ (() => {
+
+eval("// $(\".button.rus_lang\").click(function(){\r\n//     //alert(\"Нажата кнопка АНГЛИЙСКИЙ\");\r\n//     var rusLang = document.getElementsByClassName(\"rus_lang\");\r\n//     var enLang = document.getElementsByClassName(\"en_lang\");\r\n//     for (i = 0; i < enLang.length; i++) {\r\n//       enLang[i].style.position = \"static\";\r\n//       rusLang[i].style.position = \"absolute\";\r\n//       rusLang[i].style.top = \"-9999px\";\r\n//       rusLang[i].style.left = \"-9999px\";\r\n//     }\r\n//   });\r\n\r\n//     $(\".button.en_lang\").click(function(){\r\n//       //alert(\"Нажата кнопка АНГЛИЙСКИЙ\");\r\n//       var rusLang = document.getElementsByClassName(\"rus_lang\");\r\n//       var enLang = document.getElementsByClassName(\"en_lang\");\r\n//       for (i = 0; i < enLang.length; i++) {\r\n//         rusLang[i].style.position = \"static\";\r\n//         enLang[i].style.position = \"absolute\";\r\n//         enLang[i].style.top = \"-9999px\";\r\n//         enLang[i].style.left = \"-9999px\";\r\n//       }\r\n//   });\r\n\r\n// function onEntry(entry) {\r\n//     entry.forEach(change => {\r\n//       if (change.isIntersecting) {\r\n//       change.target.classList.add('.element-show');\r\n//       }\r\n//     });\r\n//   }\r\n  \r\n//   let options = {\r\n//     threshold: [0.5] };\r\n//   let observer = new IntersectionObserver(onEntry, options);\r\n//   let elements = document.querySelectorAll('.element-animation');\r\n  \r\n//   for (let elm of elements) {\r\n//     observer.observe(elm);\r\n//   }\r\n\r\n// const animItems = document.querySelector\n\n//# sourceURL=webpack:///./js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./js/main.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        })
+    ]
+};
